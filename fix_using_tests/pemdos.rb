7f6374bf10a6ab4a3f42f837c83f776747b273b6
Alexander Grey <alecgrey.coding@gmail.com>
2020-08-05 19:25:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,7 +4,10 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     binding.pry
-    10 * "s" + string
+    10.times do
+      string = "s" + string
+    end
+    string
   else
     string
   end
